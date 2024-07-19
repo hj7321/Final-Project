13bf6f4fc221dbb273d6aaac496e43b1c30d9b94
hj7321 <oboki2001@naver.com>
2024-07-19 10:07:35 +0900

    add: 프로젝트 초기 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        main: "#536493",
+        sub: "#667BC6"
+      }
     },
   },
   plugins: [],
