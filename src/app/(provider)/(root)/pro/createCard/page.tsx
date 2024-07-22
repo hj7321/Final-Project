4958f83ef36614cd8b3beac38fbea61bd051bd19
hyowls <hhvucyh@gmail.com>
2024-07-22 14:30:08 +0900

    update: createCard

--- src/app/(provider)/(root)/pro/createCard/page.tsx
+++ src/app/(provider)/(root)/pro/createCard/page.tsx
@@ -1,7 +1,18 @@
 export default function CreateCard() {
   return (
-    <div>
-      전문가 의뢰 게시물 생성 페이지
+    <div className="bg-red-200 max-w-[1240px] mx-auto">
+      <h1>전문가 의뢰 등록하기</h1>
+      <div>
+        <input type="text" className="w-full border-4 p-4" placeholder="제목을 입력해주세요." />
+      </div>
+      <div>
+        <div>
+          <p>언어 선택(중복가능)</p>
+        </div>
+        <div>
+          
+        </div>
+      </div>
     </div>
   )
 }
