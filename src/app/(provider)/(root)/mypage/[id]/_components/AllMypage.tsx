559dec912f350acfe5b2275bf4f07cd4e8af87f6
dongkyusq <hasd789@naver.com>
2024-07-22 15:12:34 +0900

    fix: css수정

--- src/app/(provider)/(root)/mypage/[id]/_components/AllMypage.tsx
+++ src/app/(provider)/(root)/mypage/[id]/_components/AllMypage.tsx
@@ -29,14 +29,16 @@ export default function AllMypage() {
     <div className="flex flex-col h-screen max-w-[80%] m-auto bg-white ">
       <div className="flex flex-1">
         <aside className="w-64  p-4">
-          <div className="flex flex-col items-center mb-6  border border-main p-4 rounded">
-            <div className="w-24 h-24 bg-black rounded-full mb-4"></div>
-            <div className="text-black font-bold border mb-4">닉네임</div>
+          <div className="flex flex-col items-center mb-6   p-4 rounded">
+            <div className="w-[180px] h-[180px] bg-black rounded-full mb-4"></div>
+            <div className="text-black font-bold mb-4">닉네임</div>
 
-            <button className="mb-2 px-4 py-2 text-black rounded border  border-main bg-white">프로필 수정하기</button>
-            <button className="px-4 py-2 text-black rounded border border-main bg-white">전문가로 전환</button>
+            <button className="mb-2 px-4  w-[244px] h-[36px] text-white   rounded-s  bg-black ">프로필 수정하기</button>
+            <button className="mb-2 px-4  w-[244px] h-[36px] text-white rounded  bg-black">전문가로 전환</button>
           </div>
-          <ul className="space-y-4 mb-[10px] ">
+          <ul className="space-y-4 mt-[64px]">
+            <li className="text-[20px] font-bold">나의 활동</li>
+
             <li className="text-gray-700  cursor-pointer" onClick={() => setActiveComponent('BookMark')}>
               찜한 목록
             </li>
@@ -46,16 +48,16 @@ export default function AllMypage() {
             <li className="text-gray-700  cursor-pointer" onClick={() => setActiveComponent('MyCommentList')}>
               내가 쓴 댓글
             </li>
-            <li className="text-gray-700  cursor-pointer" onClick={() => setActiveComponent('Portfolio')}>
-              포트폴리오 수정
-            </li>
             <li className="text-gray-700  cursor-pointer" onClick={() => setActiveComponent('AccountList')}>
               거래내역
             </li>
+            <li className="text-gray-700  cursor-pointer" onClick={() => setActiveComponent('Portfolio')}>
+              포트폴리오 수정
+            </li>
           </ul>
         </aside>
-        <main className="flex-1 p-8 border border-main">
-          <h1 className="flex mx-[40%] text-2xl font-bold mt-[40px] w-full ">{renderComponent()}</h1>
+        <main className="flex-1 p-8">
+          <h1 className="flex  text-2xl font-bold mt-[40px] w-full ">{renderComponent()}</h1>
         </main>
       </div>
     </div>
