1b1b941b03792bad34f02ac30b3f626672068edf
dongkyusq <hasd789@naver.com>
2024-07-23 12:05:01 +0900

    bugfix: footer 침범 오류 해결

--- src/app/(provider)/(root)/mypage/[id]/_components/MyPostList.tsx
+++ src/app/(provider)/(root)/mypage/[id]/_components/MyPostList.tsx
@@ -9,11 +9,24 @@ export default function MyPostList() {
       <div className="space-y-4  ">
         {postDumy.slice(0, 4).map((post) => (
           <div key={post.id} className="bg-white p-4 border border-gray-400 rounded-2xl">
-            <h3 className="font-bold text-[20px] ml-8 mb-8">{post.title}</h3>
-            <p className=" text-[16px] ml-8 mb-6">{post.content}</p>
-            <span className="text-gray-500 text-[14px] ml-8">{post.date}</span>
-            <span className="text-gray-500 text-[14px]"> view:{post.view}</span>
-            <span className="text-gray-500 text-[14px]"> like:{post.like}</span>
+            <h3 className="font-bold text-[20px] ml-8 mb-8 mt-6">{post.title}</h3>
+            <p
+              className=" text-[16px] ml-8 mb-6 mr-5"
+              style={{
+                display: '-webkit-box',
+                WebkitLineClamp: 2,
+                WebkitBoxOrient: 'vertical',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis'
+              }}
+            >
+              {post.content}
+            </p>
+            <p className="ml-8 mb-3">
+              <span className="text-gray-500 text-[14px] mr-10  ">{post.date}</span>
+              <span className="text-gray-500 text-[14px] mr-10"> view:{post.view}</span>
+              <span className="text-gray-500 text-[14px]"> like:{post.like}</span>
+            </p>
           </div>
         ))}
       </div>
