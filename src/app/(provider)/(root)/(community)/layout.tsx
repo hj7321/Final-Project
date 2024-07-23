a7cc8b6d306023bc16766f3206ca0baceff76e5e
minseo <mensung6@gmail.com>
2024-07-23 19:37:11 +0900

    move: 파일 정리 외

--- src/app/(provider)/(root)/(community)/layout.tsx
+++ src/app/(provider)/(root)/(community)/layout.tsx
@@ -1,3 +1,9 @@
-export default function CommunityLayout() {
-  return <div>커뮤니티 페이지 레이아웃</div>;
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function CommunityLayout({ children }: LayoutProps) {
+  return <div>{children}</div>;
 }
