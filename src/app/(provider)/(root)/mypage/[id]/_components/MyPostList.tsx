e763a5b759bc22ed370cc2bf4e7ff0673fbaf24a
dongkyusq <hasd789@naver.com>
2024-07-23 11:33:41 +0900

    update:마이페이지 세로 컴포넌트 글 작성완료, 레이아웃 문제 해결 중

--- src/app/(provider)/(root)/mypage/[id]/_components/MyPostList.tsx
+++ src/app/(provider)/(root)/mypage/[id]/_components/MyPostList.tsx
@@ -1,7 +1,22 @@
+import { postDumy } from './DumyData';
+
 export default function MyPostList() {
   return (
-    <div>
-      <h1>내가 쓴 글 목록 페이지입니다</h1>
-    </div>
+    <section className="container mx-auto px-4 py-8 min-h-screen">
+      <div className=" mb-10">
+        <h2 className="text-2xl font-bold">내가 쓴 글</h2>
+      </div>
+      <div className="space-y-4  ">
+        {postDumy.slice(0, 4).map((post) => (
+          <div key={post.id} className="bg-white p-4 border border-gray-400 rounded-2xl">
+            <h3 className="font-bold text-[20px] ml-8 mb-8">{post.title}</h3>
+            <p className=" text-[16px] ml-8 mb-6">{post.content}</p>
+            <span className="text-gray-500 text-[14px] ml-8">{post.date}</span>
+            <span className="text-gray-500 text-[14px]"> view:{post.view}</span>
+            <span className="text-gray-500 text-[14px]"> like:{post.like}</span>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
