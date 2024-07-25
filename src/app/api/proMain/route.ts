6847955cc54cac5f46500478d66fc3ca1a4774c3
hyowls <hhvucyh@gmail.com>
2024-07-25 16:16:16 +0900

    update:proMain 언어별 필터링

--- src/app/api/proMain/route.ts
+++ src/app/api/proMain/route.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
+
+
 export async function GET() {
   try {
     const supabase = createClient()
