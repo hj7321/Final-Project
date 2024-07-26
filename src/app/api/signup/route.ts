4ca95714fb48e443243417c93d96e256f3e0a9e1
hj7321 <oboki2001@naver.com>
2024-07-26 19:34:47 +0900

    delete: Users 테이블에 회원정보 저장하는 로직은 트리거로 해결, supabase 자체가 회원가입하면 자동 로그인이 되므로 로그인 로직 삭제

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -7,49 +7,26 @@ export async function POST(request: Request) {
 
   // 서버 측에서도 supabase에 데이터 저장하기 전에 검증 과정 필요!!
 
-  // 1) auth 테이블에 회원정보 저장(회원가입)
-  const { error: signUpError } = await supabase.auth.signUp({
+  const { data: signUpData, error: signUpError } = await supabase.auth.signUp({
     email,
     password,
     options: {
       data: {
         displayName: nickname,
-        name,
-        birth
+        name: nickname,
+        full_name: name,
+        birth,
+        avatar_url: null
       }
     }
   });
 
   if (signUpError) {
+    console.log(signUpError);
     console.log('회원가입 실패: ', signUpError.message);
     return Response.json({ errorMsg: signUpError.message });
-  } else console.log('회원가입 성공');
-
-  // 2) Users 테이블에 회원정보 저장
-  const { data: usersInsertData, error: usersInsertError } = await supabase.from('Users').insert({
-    email,
-    nickname,
-    birth,
-    name
-  });
-
-  if (usersInsertError) {
-    console.log('Users 테이블에 회원정보 저장 실패: ', usersInsertError.message);
-    return Response.json({ errorMsg: usersInsertError.message });
-  } else console.log('Users 테이블에 회원정보 저장 성공');
-
-  // 3) 회원가입 하면 자동 로그인
-  const { data: loginData, error: loginError } = await supabase.auth.signInWithPassword({
-    email,
-    password
-  });
-
-  if (loginError) {
-    console.log('로그인 실패: ', loginError.message);
-    return Response.json({ errorMsg: loginError.message });
   } else {
-    console.log('로그인 성공');
-    console.log(loginData);
-    return Response.json({ userData: loginData });
+    console.log('회원가입 성공');
+    return Response.json({ userData: signUpData });
   }
 }
