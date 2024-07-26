0031277dbbbd89b5fa167ca4a73d3951cd29d2ec
hj7321 <oboki2001@naver.com>
2024-07-26 19:30:55 +0900

    update: 로그인 상태 초기화하는 함수 추가, 인터페이스 추가

--- src/zustand/authStore.ts
+++ src/zustand/authStore.ts
@@ -1,4 +1,6 @@
-import { AuthState } from '@/types/auth.type';
+import { Users } from '@/types/type';
+import { createClient } from '@/utils/supabase/client';
+import { UserMetadata } from '@supabase/supabase-js';
 import { create, StoreApi, UseBoundStore } from 'zustand';
 
 // 전역 상태에서 정의해야 할 것
@@ -10,14 +12,57 @@ import { create, StoreApi, UseBoundStore } from 'zustand';
 // 5. 사용자 고유 아이디 설정 함수: userId를 현재 로그인 되어 있는 사용자 고유 아이디로 설정함
 // 6. 사용자 개인 정보 설정 함수: userData를 현재 로그인 되어 있는 사용자 개인 정보로 설정함
 
-const useAuthStore: UseBoundStore<StoreApi<AuthState>> = create<AuthState>((set) => ({
-  isLogin: false,
-  userId: null,
-  userData: null,
-  login: () => set({ isLogin: true }),
-  logout: () => set({ isLogin: false, userId: null, userData: null }),
-  setUserId: (userId) => set({ userId }),
-  setUserData: (userData) => set({ userData })
-}));
+interface AuthState {
+  isLoading: boolean;
+  isLogin: boolean;
+  userId: Users['id'] | null;
+  userData: UserMetadata | null;
+  login: () => void;
+  logout: () => void;
+  setUserId: (userId: Users['id'] | null) => void;
+  setUserData: (userData: UserMetadata | null) => void;
+  initializeAuthState: () => Promise<void>;
+}
+
+const useAuthStore: UseBoundStore<StoreApi<AuthState>> = create<AuthState>((set) => {
+  // 로그인 상태를 초기화하는 함수
+  const initializeAuthState = async () => {
+    set({ isLoading: true });
+
+    const supabase = createClient();
+    const {
+      data: { session }
+    } = await supabase.auth.getSession();
+
+    // const session = await fetch('/api/auth', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // }).then((res) => res.json());
+    console.log(session);
+
+    set({
+      isLogin: !!session,
+      userId: session?.user?.id || null,
+      userData: session?.user?.user_metadata || null,
+      isLoading: false
+    });
+  };
+
+  initializeAuthState();
+
+  return {
+    isLoading: true,
+    isLogin: false,
+    userId: null,
+    userData: null,
+    login: () => set({ isLogin: true }),
+    logout: () => set({ isLogin: false, userId: null, userData: null }),
+    setUserId: (userId) => set({ userId }),
+    setUserData: (userData) => set({ userData }),
+    initializeAuthState
+  };
+});
 
 export default useAuthStore;
