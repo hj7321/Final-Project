5980aa95bef8ba704e2af08b1a0859b6b7398bda
hyowls <hhvucyh@gmail.com>
2024-08-02 13:57:05 +0900

    update:마크다운 툴바 수정

--- src/app/(provider)/(root)/pro/createCard/_components/DescriptionInput.tsx
+++ src/app/(provider)/(root)/pro/createCard/_components/DescriptionInput.tsx
@@ -1,24 +1,30 @@
 'use client'
 import MDEditor, { commands } from '@uiw/react-md-editor'
 import '@uiw/react-md-editor/markdown-editor.css'
-
+import '../../../../../../css/mdStyle.css'
 interface DescriptionInputProps {
   description : string
-  setDescription : (description : string) => void
+  setDescription : (description : any) => void
 }
 
 export default function DescriptionInput({description, setDescription} : DescriptionInputProps) {
   return (
     <div className="border-2 border-slate-400 mb-[20px] rounded-md" data-color-mode='light'>
-      {/* <textarea
-        name="text"
-        id="text"
-        className="w-full h-[500px] p-5"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      ></textarea> */}
-      <MDEditor height={700} value={description} onChange={setDescription} className="py-5"
-        commands={[]}
+      <MDEditor height={700} value={description} onChange={setDescription}
+        commands={[
+          commands.title1,
+          commands.title2,
+          commands.title3,
+          commands.divider,
+          commands.bold,
+          commands.italic,
+          commands.divider,
+          commands.orderedListCommand,
+          commands.unorderedListCommand,
+          commands.divider,
+          commands.code,
+          commands.quote
+        ]}
       />
     </div>
   )
