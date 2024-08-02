29f84be24f984eb3efb4ffa8fdc81701453d6e16
hj7321 <oboki2001@naver.com>
2024-08-02 20:33:42 +0900

    fix: 충돌 에러 해결

--- src/app/(provider)/(root)/pro/createCard/_components/DescriptionInput.tsx
+++ src/app/(provider)/(root)/pro/createCard/_components/DescriptionInput.tsx
@@ -1,24 +1,20 @@
-<<<<<<< HEAD
-'use client'
-import MDEditor, { commands } from '@uiw/react-md-editor'
-import '@uiw/react-md-editor/markdown-editor.css'
-import '../../../../../../css/mdStyle.css'
-=======
 'use client';
 import MDEditor, { commands } from '@uiw/react-md-editor';
 import '@uiw/react-md-editor/markdown-editor.css';
->>>>>>> 7a8946630bb91c7e3a833cf04b53f2619392a693
+import '../../../../../../css/mdStyle.css';
 
 interface DescriptionInputProps {
-  description : string
-  setDescription : (description : any) => void
+  description: string;
+  setDescription: (description: any) => void;
 }
 
 export default function DescriptionInput({ description, setDescription }: DescriptionInputProps) {
   return (
-<<<<<<< HEAD
-    <div className="border-2 border-slate-400 mb-[20px] rounded-md" data-color-mode='light'>
-      <MDEditor height={700} value={description} onChange={setDescription}
+    <div className="border-2 border-slate-400 mb-[20px] rounded-md" data-color-mode="light">
+      <MDEditor
+        height={700}
+        value={description}
+        onChange={setDescription}
         commands={[
           commands.title1,
           commands.title2,
@@ -34,17 +30,6 @@ export default function DescriptionInput({ description, setDescription }: Descri
           commands.quote
         ]}
       />
-=======
-    <div className="border-2 border-slate-400 mb-[20px] rounded-md" data-color-mode="light">
-      {/* <textarea
-        name="text"
-        id="text"
-        className="w-full h-[500px] p-5"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      ></textarea> */}
-      <MDEditor height={700} value={description} onChange={setDescription} className="py-5" commands={[]} />
->>>>>>> 7a8946630bb91c7e3a833cf04b53f2619392a693
     </div>
   );
 }
