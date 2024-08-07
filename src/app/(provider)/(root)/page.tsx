e4cf0417e37cd182989a62166c16d43fc07bf868
hwangseunghyun <hwamg123@naver.com>
2024-08-07 20:14:19 +0900

    fix : 홈페이지 마진수정

--- src/app/(provider)/(root)/page.tsx
+++ src/app/(provider)/(root)/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
   return (
     <main className="snap-y scroll-smooth">
       {/* 메인베너 */}
-      <section className="w-full md:h-[calc(100vh-75px)] mb-16 md:mb-64">
+      <section className="w-full md:h-[calc(100vh-75px)] mb-2 md:mb-64">
         <div className="block md:hidden w-full h-auto bg-cover bg-center">
           <img src="/mobileMainBanner.svg" alt="모바일 메인베너" className="w-full h-auto object-cover" />
         </div>
@@ -59,7 +59,7 @@ export default function Home() {
       </section>
 
       {/* 언어별 카테고리 및 커뮤니티 섹션 */}
-      <section className="w-full flex flex-col min-h-[calc(100vh-75px)] snap-start mb-16 md:mb-64">
+      <section className="w-full flex flex-col min-h-[calc(100vh-75px)] snap-start mb-12 md:mb-64">
         <div className="bg-white py-4 flex-shrink-0">
           <div className="container mx-auto px-4 md:px-16 h-full">
             <h2 className="text-xl font-bold mb-4">언어별 카테고리</h2>
