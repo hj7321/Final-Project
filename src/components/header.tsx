e8074805584cb06f1c7c655fc28ff1ff2c7d67d4
hj7321 <oboki2001@naver.com>
2024-08-07 20:06:24 +0900

    update, style: 헤더에 햄버거 컴포넌트 추가 및 반응형 스타일 구현

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState, Suspense } from 'react';
+import React, { useEffect, useState, Suspense, useRef } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import useAuthStore from '@/zustand/authStore';
@@ -9,6 +9,7 @@ import { createClient } from '@/utils/supabase/client';
 import Image from 'next/image';
 import Cookies from 'js-cookie';
 import { useQuery } from '@tanstack/react-query';
+import Hamburger from './Hamburger';
 
 const buttonStyle = 'w-[100px] h-[40px] px-[16px] py-[8px] rounded-[8px] text-center';
 const linkStyle = 'text-grey-700 hover:text-primary-500 hover:font-bold px-[12px] py-[8px]';
@@ -16,6 +17,7 @@ const linkStyle = 'text-grey-700 hover:text-primary-500 hover:font-bold px-[12px
 export default function Header() {
   const [searchInput, setSearchInput] = useState('');
   const [number, setNumber] = useState<number>(0);
+  const [open, setOpen] = useState<boolean>(false);
   const [session, setSession] = useState<string | null | undefined>(null);
   const router = useRouter();
   const { isLogin, logout, userId, initializeAuthState } = useAuthStore();
@@ -88,110 +90,123 @@ export default function Header() {
   };
 
   const handleOpenMenu = () => {
-    // 이걸 누르면 메뉴가 나오고, 화면 다른 곳을 누르면 다시 들어가야 함
+    setOpen(true);
   };
+
+  const handleCloseMenu = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (open) document.addEventListener('click', handleCloseMenu);
+    else document.removeEventListener('click', handleCloseMenu);
+
+    return () => {
+      document.removeEventListener('click', handleCloseMenu);
+    };
+  }, [open]);
 
   return (
     <>
-      {
-        <header
-          className={clsx(
-            'sticky top-0 z-50 bg-white flex h-[56px] md:h-[72px] px-[16px] md:px-[120px] py-[16px] border border-grey-100',
-            isAuthPage && 'hidden md:flex'
-          )}
-        >
-          <div className="w-full flex justify-between items-center md:hidden">
-            <button onClick={handleOpenMenu}>
-              <Image src="/hamburger.svg" alt="메뉴 아이콘" width={24} height={24} />
-            </button>
+      <header
+        className={clsx(
+          'sticky top-0 z-40 flex h-[56px] md:h-[72px] px-[16px] md:px-[120px] py-[16px] border border-grey-100',
+          isAuthPage && 'hidden md:flex'
+        )}
+      >
+        <div className="w-full flex justify-between items-center md:hidden">
+          <button onClick={handleOpenMenu}>
+            <Image src="/hamburger.svg" alt="메뉴 아이콘" width={24} height={24} />
+            {open && <Hamburger onClose={handleCloseMenu} />}
+          </button>
+          <Link href="/">
+            <div className="w-40 h-16 flex items-center justify-center">
+              {number === 0 ? (
+                <Image src="/logo_eng.svg" alt="영어 로고" width={120} height={34} />
+              ) : (
+                <Image src="/logo_kor.svg" alt="한국어 로고" width={120} height={34} />
+              )}
+            </div>
+          </Link>
+          <button>
+            <Image src="/searchIcon.svg" alt="돋보기 아이콘" width={24} height={24} />
+          </button>
+        </div>
+
+        <div className="w-full justify-between items-center hidden md:flex">
+          <div className="flex items-center gap-[32px]">
             <Link href="/">
               <div className="w-40 h-16 flex items-center justify-center">
                 {number === 0 ? (
-                  <Image src="/logo_eng.svg" alt="영어 로고" width={120} height={34} />
+                  <Image src="/logo_eng.svg" alt="영어 로고" width={165} height={55} />
                 ) : (
-                  <Image src="/logo_kor.svg" alt="한국어 로고" width={120} height={34} />
+                  <Image src="/logo_kor.svg" alt="한국어 로고" width={165} height={55} />
                 )}
               </div>
             </Link>
-            <button>
+            <nav className="ml-4 space-x-[12px]">
+              <Link href="/qna" className={linkStyle}>
+                Q&A
+              </Link>
+              <Link href="/insight" className={linkStyle}>
+                인사이트
+              </Link>
+              <Link href="/pro" className={linkStyle}>
+                전문가 의뢰
+              </Link>
+            </nav>
+          </div>
+
+          <div className="w-[340px] h-[40px] flex justify-between bg-grey-50 rounded-[20px] px-[16px] py-[8px]">
+            <input
+              type="text"
+              placeholder="도움이 필요한 언어, 주제를 찾아보세요."
+              className="bg-grey-50 outline-none w-[250px] h-[24px] placeholder-grey-200 placeholder:text-[15px] text-black text-[15px]"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
+            />
+            <button onClick={handleSearch}>
               <Image src="/searchIcon.svg" alt="돋보기 아이콘" width={24} height={24} />
             </button>
           </div>
-
-          <div className="w-full justify-between items-center hidden md:flex">
-            <div className="flex items-center gap-[32px]">
-              <Link href="/">
-                <div className="w-40 h-16 flex items-center justify-center">
-                  {number === 0 ? (
-                    <Image src="/logo_eng.svg" alt="영어 로고" width={165} height={55} />
-                  ) : (
-                    <Image src="/logo_kor.svg" alt="한국어 로고" width={165} height={55} />
-                  )}
-                </div>
-              </Link>
-              <nav className="ml-4 space-x-[12px]">
-                <Link href="/qna" className={linkStyle}>
-                  Q&A
+          {isLogin ? (
+            <div className="flex items-center gap-[24px]">
+              <div className="flex gap-[12px]">
+                <Image src="/alarm_comment.svg" alt="댓글 알림 아이콘" width={24} height={24} />
+                <Image src="/alarm_chat.svg" alt="채팅 알림 아이콘" width={24} height={24} />
+              </div>
+              <div className="flex items-center gap-[24px]">
+                <Link href={`/mypage/${userId}`} className="text-grey-500 hover:text-grey-700">
+                  <b className="text-primary-500 hover:text-primary-700">{Users?.data?.nickname}</b>님
                 </Link>
-                <Link href="/insight" className={linkStyle}>
-                  인사이트
-                </Link>
-                <Link href="/pro" className={linkStyle}>
-                  전문가 의뢰
-                </Link>
-              </nav>
+                <button
+                  onClick={handleLogout}
+                  className={clsx(buttonStyle, 'bg-grey-200 hover:bg-grey-300 text-white')}
+                >
+                  로그아웃
+                </button>
+              </div>
             </div>
-
-            <div className="w-[340px] h-[40px] flex justify-between bg-grey-50 rounded-[20px] px-[16px] py-[8px]">
-              <input
-                type="text"
-                placeholder="도움이 필요한 언어, 주제를 찾아보세요."
-                className="bg-grey-50 outline-none w-[250px] h-[24px] placeholder-grey-200 placeholder:text-[15px] text-black text-[15px]"
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value)}
-                onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
-              />
-              <button onClick={handleSearch}>
-                <Image src="/searchIcon.svg" alt="돋보기 아이콘" width={24} height={24} />
+          ) : (
+            <div className="flex items-center gap-[16px] text-[16px]">
+              <button
+                onClick={goToLoginPage}
+                className={clsx(buttonStyle, 'border border-primary-500 hover:bg-primary-50 text-primary-500')}
+              >
+                로그인
+              </button>
+              <button
+                onClick={goToSignUpPage}
+                className={clsx(buttonStyle, 'bg-primary-500 hover:bg-primary-700 text-white')}
+              >
+                회원가입
               </button>
             </div>
-            {isLogin ? (
-              <div className="flex items-center gap-[24px]">
-                <div className="flex gap-[12px]">
-                  <Image src="/alarm_comment.svg" alt="댓글 알림 아이콘" width={24} height={24} />
-                  <Image src="/alarm_chat.svg" alt="채팅 알림 아이콘" width={24} height={24} />
-                </div>
-                <div className="flex items-center gap-[24px]">
-                  <Link href={`/mypage/${userId}`} className="text-grey-500 hover:text-grey-700">
-                    <b className="text-primary-500 hover:text-primary-700">{Users?.data?.nickname}</b>님
-                  </Link>
-                  <button
-                    onClick={handleLogout}
-                    className={clsx(buttonStyle, 'bg-grey-200 hover:bg-grey-300 text-white')}
-                  >
-                    로그아웃
-                  </button>
-                </div>
-              </div>
-            ) : (
-              <div className="flex items-center gap-[16px] text-[16px]">
-                <button
-                  onClick={goToLoginPage}
-                  className={clsx(buttonStyle, 'border border-primary-500 hover:bg-primary-50 text-primary-500')}
-                >
-                  로그인
-                </button>
-                <button
-                  onClick={goToSignUpPage}
-                  className={clsx(buttonStyle, 'bg-primary-500 hover:bg-primary-700 text-white')}
-                >
-                  회원가입
-                </button>
-              </div>
-            )}
-          </div>
-        </header>
-      }
+          )}
+        </div>
+      </header>
+      {open && <div className="fixed inset-0 bg-grey-900 bg-opacity-50 z-30" onClick={handleCloseMenu}></div>}
     </>
   );
 }
