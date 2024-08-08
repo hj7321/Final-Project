b319163fad75887dff4b8a0a822197d0f9a75939
hyowls <hhvucyh@gmail.com>
2024-08-08 21:07:03 +0900

    style:반응형 작업/proMain

--- src/app/(provider)/(root)/pro/page.tsx
+++ src/app/(provider)/(root)/pro/page.tsx
@@ -106,12 +106,12 @@ export default function ProMainPage() {
   };
   return (
     <div className="mx-auto flex-col justify-center items-center">
-      <div className="flex flex-row justify-end mt-[15px] md:w-[85%] w-full">
+      <div className="hidden md:block flex flex-row justify-end mt-[15px] md:w-auto items-end mx-auto w-full 2xl:w-[85%]">
         {!isPro === true ? (
           <div className=""></div>
         ) : (
           <button
-            className="bg-primary-500 hover:bg-primary-600 md:px-5 md:py-3 md:mt-3 md:mr-[0px] px-3 py-2 mt-1 mr-4 flex flex-row justify-center items-center rounded-full"
+            className="md:ml-[85%] bg-primary-500 hover:bg-primary-600 md:px-5 md:py-3 md:mt-3 md:mr-[0px] px-3 py-2 mt-1 mr-4 flex flex-row justify-center items-center rounded-full"
             onClick={handleNavigation}
           >
             <div className='md:w-[24px] md:h-[24px] w-[16px] h-[16px]'>
@@ -127,12 +127,12 @@ export default function ProMainPage() {
                 />
               </svg>
             </div>
-            <div className="text-white mx-[2px] md:text-base text-[12px]">등록하기</div>
+            <div className="text-white mx-[2px] md:text-base text-[12px]">홍보하기</div>
           </button>
         )}
       </div>
       {/* 언어별 카테고리 영역 */}
-      <div className="md:mt-[30px] mt-[20px] mx-auto overflow-hidden">
+      <div className=" md:mt-[30px] mt-[20px] mx-auto overflow-hidden">
         <ul className="flex flex-row justify-start items-center max-w-7xl mx-auto lg:justify-between lg:flex-wrap lg:overflow-visible overflow-x-auto scrollbar-hide">
           {CodeCategories.map((lang) => (
             <li
@@ -157,9 +157,39 @@ export default function ProMainPage() {
           ))}
         </ul>
       </div>
+      
+      
+      <div className="md:hidden flex flex-row justify-end mt-[15px] md:w-[85%] w-[330px] mx-auto items-center">
+        {!isPro === true ? (
+          <div className=""></div>
+        ) : (
+          <button
+            className="w-full bg-primary-500 hover:bg-primary-600 md:px-5 md:py-3 md:mt-3 md:mr-[0px] px-3 py-2 mt-1 flex flex-row justify-center items-center rounded-lg"
+            onClick={handleNavigation}
+          >
+            <div className='w-[24px] h-[24px]'>
+              <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12.2536 6.47487L9.77877 8.94975L5.17441 13.5541C4.95175 13.7768 4.81748 14.0727 4.79653 14.3869L4.5743 17.7204C4.52198 18.5052 5.1731 19.1563 5.95789 19.104L9.29139 18.8817C9.60559 18.8608 9.9015 18.7265 10.1242 18.5039L14.7285 13.8995L17.2034 11.4246M12.2536 6.47487L13.8093 4.91924C14.317 4.41156 15.1401 4.41156 15.6478 4.91924L18.759 8.03051C19.2667 8.53819 19.2667 9.3613 18.759 9.86899L17.2034 11.4246M12.2536 6.47487L17.2034 11.4246"
+                  stroke="white"
+                />
+                <path
+                  d="M18.759 8.03051L15.6478 4.91924C15.1401 4.41156 14.317 4.41156 13.8093 4.91924L13.1729 5.55563C12.6652 6.06332 12.6652 6.88643 13.1729 7.39411L16.2842 10.5054C16.7918 11.0131 17.6149 11.0131 18.1226 10.5054L18.759 9.86899C19.2667 9.3613 19.2667 8.53819 18.759 8.03051Z"
+                  fill="white"
+                  stroke="white"
+                />
+              </svg>
+            </div>
+            <div className="text-white mx-[2px] text-base">홍보하기</div>
+          </button>
+        )}
+      </div>
+
+
+
 
       {/* 의뢰 서비스 리스트 */}
-      <div className="lg:max-w-[1440px] md:mx-auto flex flex-row flex-wrap mb-[70px] mt-[30px] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center lg:justify-start">
+      <div className="lg:max-w-[1440px] md:mx-auto flex flex-row flex-wrap mb-[70px] mt-[10px] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center lg:justify-start">
         {Array.isArray(filteredPosts) && filteredPosts.length > 0 ? (
           filteredPosts.map((post) => (
             <Link href={`pro/proDetail/${post.id}`} key={post.id} className="grid grid-cols-1 mx-auto">
