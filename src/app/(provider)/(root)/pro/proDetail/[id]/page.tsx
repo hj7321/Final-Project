5336cd48cd156374221b7f2c79988840e1f7fe7e
hyowls <hhvucyh@gmail.com>
2024-08-08 17:08:45 +0900

    update: default text

--- src/app/(provider)/(root)/pro/proDetail/[id]/page.tsx
+++ src/app/(provider)/(root)/pro/proDetail/[id]/page.tsx
@@ -188,9 +188,7 @@ export default function ProDetail() {
             </div>
             <div className="mx-auto md:mt-5 my-3">
               <p className="md:w-[30ch] w-full md:min-h-[50px] md:line-clamp-3 md:text-base text-xs text-grey-500 line-clamp-2">
-                유저에 대한 한 줄 소개글이 들어감. 유저 테이블에 필요할듯 유저에 대한 한 줄 소개글이 들어감. 유저
-                테이블에 필요할듯 유저에 대한 한 줄 소개글이 들어감. 유저 테이블에 필요할듯 유저에 대한 한 줄 소개글이
-                들어감. 유저 테이블에 필요할듯
+                프로젝트에 대한 요구사항을 함께 논의하고, 최적의 솔루션을 제공하겠습니다. 언제든지 문의해 주세요!
               </p>
             </div>
             <div className="mx-auto md:w-[85%] w-full md:mt-5 my-2 flex flex-row justify-between items-center md:flex-col">
