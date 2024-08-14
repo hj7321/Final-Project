be6ce017a952fd53ba855c798bf4036353c06a70
dongkyusq <hasd789@naver.com>
2024-08-14 17:18:49 +0900

    update:리뷰 작성, 삭제 구현

--- src/app/(provider)/(root)/mypage/[id]/_components/DetailReview.tsx
+++ src/app/(provider)/(root)/mypage/[id]/_components/DetailReview.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { RequestReviews } from '@/types/type';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import Image from 'next/image';
 import { useParams } from 'next/navigation';
 import StarRating from './StarRating';
 import { useState } from 'react';
+import { Confirm, Notify } from 'notiflix';
+import AddReview from './AddReview';
 
 interface PortfolioData {
   id: string;
@@ -19,6 +21,17 @@ interface PortfolioData {
   post_img: string[];
 }
 
+interface ReData {
+  id: string;
+  created_at: string;
+  user_id: string;
+  contents: string;
+  stars: number;
+  request_post_id: string;
+}
+
+type ReviewsData = Omit<RequestReviews, 'id' | 'created_at'>;
+
 type ReviewModalProps = {
   onClose: () => void;
   portfolio: PortfolioData;
@@ -26,7 +39,14 @@ type ReviewModalProps = {
 
 const DetailReview: React.FC<ReviewModalProps> = ({ onClose, portfolio }) => {
   const { id } = useParams();
+  const portfolioId = Array.isArray(portfolio.id) ? portfolio.id[0] : portfolio.id;
+  const userId = Array.isArray(id) ? id[0] : id;
+  const params = useParams();
+  const paramsId = params.id as string;
+
   const [rating, setRating] = useState(0);
+  const [content, setContent] = useState('');
+  const queryClient = useQueryClient();
 
   const imageUrl = Array.isArray(portfolio.post_img) ? portfolio.post_img[0] : '';
 
@@ -36,7 +56,7 @@ const DetailReview: React.FC<ReviewModalProps> = ({ onClose, portfolio }) => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data: RequestReviews[] = await response.json();
-    return data.filter((post) => post.user_id === id);
+    return data.filter((post) => post.user_id === id && post.request_post_id === portfolio.id);
   };
 
   const { data, isLoading, error } = useQuery<RequestReviews[]>({
@@ -45,43 +65,133 @@ const DetailReview: React.FC<ReviewModalProps> = ({ onClose, portfolio }) => {
     enabled: !!id
   });
 
+  const saveReview = async (data: ReviewsData) => {
+    const response = await fetch('/api/review', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  };
+
+  const deleteReview = async (id: string) => {
+    const response = await fetch('/api/review', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id })
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  };
+
+  const { mutate: deleteMutation } = useMutation<ReData, Error, string>({
+    mutationFn: (id) => deleteReview(id),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['porifolio', paramsId] })
+  });
+
+  const handleDelete = (id: string) => {
+    Confirm.show(
+      '리뷰 삭제',
+      '정말로 삭제하시겠습니까?',
+      '예',
+      '아니오',
+      () => {
+        Notify.failure('삭제되었습니다.');
+        try {
+          deleteMutation(id);
+        } catch (error) {
+          console.error('삭제에 실패했습니다.', error);
+        }
+      },
+      () => {
+        Notify.failure('삭제가 되지 않았습니다.');
+      },
+      {}
+    );
+  };
+
+  const { mutate: addMutation } = useMutation<ReviewsData, unknown, ReviewsData>({
+    mutationFn: (reviewData: ReviewsData) => saveReview(reviewData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['post', id] });
+      setContent(''); // textarea 값 초기화
+      Notify.success('리뷰가 성공적으로 등록되었습니다.');
+      onClose();
+    }
+  });
+
+  const handleSubmit = async () => {
+    if (!userId || !portfolio.id) {
+      alert('필수 정보가 누락되었습니다.');
+      return;
+    }
+    if (!content || rating === 0) {
+      Notify.failure('내용과 평점을 입력해주세요.');
+      return;
+    }
+
+    try {
+      const reviewData: ReviewsData = {
+        user_id: userId,
+        request_post_id: portfolio.id,
+        stars: rating,
+        contents: content
+      };
+
+      addMutation(reviewData);
+    } catch (error) {
+      console.error('리뷰 등록 중 오류가 발생했습니다:', error);
+    }
+  };
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white flex flex-col    p-6 w-[40%] h-[50%] relative overflow-auto">
-        <button onClick={onClose} className="text-grey-600 flex justify-end mr-0">
-          x
-        </button>
-        <div className="flex flex-col">
-          <div className="flex flex-row mt-[24px]">
-            {imageUrl && <Image src={imageUrl} alt="ds" width={100} height={100} className="w-[100px] h-[100px]" />}
-            <div className="flex flex-col ml-[40px]">
-              <h1 className="flex text-base  text-grey-600  font-bold ">{portfolio.lang_category}</h1>
-              <h1 className="flex text-base  text-grey-800 font-bold ">{portfolio.title}</h1>
+      {data?.length === 0 ? (
+        <AddReview onClose={onClose} portfolio={portfolio} userId={userId} addMutation={addMutation} />
+      ) : (
+        <div className="bg-white flex flex-col p-6 w-[40%] h-[350px] relative overflow-auto rounded-xl">
+          <div className="flex flex-col">
+            <button onClick={onClose} className="absolute top-2 right-4 text-grey-600 flex font-thin justify-end ">
+              x
+            </button>
+            <div className="flex flex-row mt-[24px] ">
+              {imageUrl && <Image src={imageUrl} alt="ds" width={100} height={100} className="w-[100px] h-[100px]" />}
+              <div className="flex flex-col ml-[40px]">
+                <h1 className="flex text-sm  text-grey-600  font-bold ">{portfolio.lang_category}</h1>
+                <h1 className="flex text-base  text-grey-800 font-bold ">{portfolio.title}</h1>
+              </div>
             </div>
           </div>
+
+          <div>
+            {data?.map((review) => (
+              <div key={review.id} className="mt-5">
+                <div className="flex items-center">
+                  <StarRating totalStars={5} size={30} selectedStars={review.stars} readOnly />
+                  <div className="mt-3 text-gray-400 text-sm">({review.stars})</div>
+                </div>
+                <p className="ml-1 font-semibold mt-3 text-lg text-grey-800">{review.contents}</p>
+                <p className="mt-3 text-sm text-gray-400">작성일: {review.created_at.slice(0, 10)}</p>
+                <div className="flex justify-end">
+                  <button className="bg-primary-500 text-white text-base font-normal mt-2 px-4 py-2 rounded-md shadow-sm mr-0 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    리뷰 수정
+                  </button>
+                  <button
+                    onClick={() => handleDelete(review.id)}
+                    className="bg-grey-200 ml-3 text-white text-base font-normal mt-2 px-4 py-2 rounded-md shadow-sm mr-0 hover:bg-grey-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-grey-600"
+                  >
+                    리뷰 삭제
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        {data?.length === 0 ? (
-          <>
-            <div className="flex mt-5">
-              <StarRating totalStars={5} size={40} onRatingChange={(newRating) => setRating(newRating)} />
-              <div className="mt-3 text-gray-400 text-sm">({rating})</div>
-            </div>
-            <div>
-              <textarea
-                placeholder="솔직한 리뷰는 이용자와 모두에게 도움이 됩니다."
-                className="border border-grey-300 rounded-lg font-medium text-base mt-10 h-[100px] w-full p-2"
-                rows={4}
-              />
-            </div>
-          </>
-        ) : (
-          <>
-            <div>
-              <h1>ds</h1>
-            </div>
-          </>
-        )}
-      </div>
+      )}
     </div>
   );
 };
--- src/app/(provider)/(root)/mypage/[id]/_components/StarRating.tsx
+++ src/app/(provider)/(root)/mypage/[id]/_components/StarRating.tsx
@@ -3,17 +3,27 @@ import React, { useState } from 'react';
 type StarRatingProps = {
   totalStars?: number;
   size?: number; // 별의 크기
+  selectedStars?: number; // 선택된 별의 수
   onRatingChange?: (rating: number) => void;
+  readOnly?: boolean; // 별점 변경 가능 여부
 };
 
-const StarRating: React.FC<StarRatingProps> = ({ totalStars = 5, size = 24, onRatingChange }) => {
-  const [rating, setRating] = useState(0);
+const StarRating: React.FC<StarRatingProps> = ({
+  totalStars = 5,
+  size = 24,
+  selectedStars = 0,
+  onRatingChange,
+  readOnly = false
+}) => {
+  const [rating, setRating] = useState(selectedStars);
   const [hover, setHover] = useState(0);
 
   const handleClick = (index: number) => {
-    setRating(index);
-    if (onRatingChange) {
-      onRatingChange(index);
+    if (!readOnly) {
+      setRating(index);
+      if (onRatingChange) {
+        onRatingChange(index);
+      }
     }
   };
 
@@ -23,15 +33,15 @@ const StarRating: React.FC<StarRatingProps> = ({ totalStars = 5, size = 24, onRa
         <svg
           key={index}
           onClick={() => handleClick(index + 1)}
-          onMouseEnter={() => setHover(index + 1)}
-          onMouseLeave={() => setHover(0)}
+          onMouseEnter={() => (readOnly ? null : setHover(index + 1))}
+          onMouseLeave={() => (readOnly ? null : setHover(0))}
           xmlns="http://www.w3.org/2000/svg"
           fill={index < (hover || rating) ? 'gold' : 'lightgray'}
           viewBox="0 0 24 24"
           stroke={index < (hover || rating) ? 'gold' : 'lightgray'}
           width={size} // 크기 설정
           height={size} // 크기 설정
-          style={{ cursor: 'pointer', marginRight: '5px' }}
+          style={{ cursor: readOnly ? 'default' : 'pointer', marginRight: '5px' }}
         >
           <path
             strokeLinecap="round"
