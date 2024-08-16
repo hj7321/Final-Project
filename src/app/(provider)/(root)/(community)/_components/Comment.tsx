bbb00613b7d0cbfc56b97e762944754a2aed63ea
minseo <mensung6@gmail.com>
2024-08-16 21:08:30 +0900

    pull : dev merge 외

--- src/app/(provider)/(root)/(community)/_components/Comment.tsx
+++ src/app/(provider)/(root)/(community)/_components/Comment.tsx
@@ -33,13 +33,13 @@ export default function Comment({
   return (
     <div key={comment.id}>
       {editingCommentId === comment.id ? (
-        <div className="flex space-y-2 p-4 border border-gray-300 rounded-md shadow-sm gap-1">
-          <MDEditor height={100} value={editContent} onChange={setEditContent} commands={[]} />
+        <div data-color-mode="light" className="flex space-y-2 p-4 border border-gray-300 rounded-md shadow-sm gap-1">
+          <MDEditor height={100} value={editContent} onChange={() => setEditContent} commands={[]} />
           <button onClick={() => handleSaveClick(comment.id)}>저장</button>
           <button onClick={handleCancelClick}>취소</button>
         </div>
       ) : (
-        <div>
+        <div data-color-mode="light">
           <p className="font-bold">{getUserNickname(comment.user_id)}</p>
           <MDEditor.Markdown source={comment.contents} />
           <div className="flex gap-[24px]">
