7835c074ae65ee4431d66f6d844b310f5490a507
hyowls <hhvucyh@gmail.com>
2024-08-16 17:27:00 +0900

    style: 리뷰 별점 구현

--- src/app/(provider)/(root)/pro/proDetail/[id]/_components/Reviews.tsx
+++ src/app/(provider)/(root)/pro/proDetail/[id]/_components/Reviews.tsx
@@ -1,6 +1,4 @@
-
-import Image from "next/image"
-
+import { FaStar, FaStarHalfAlt } from 'react-icons/fa'
 interface Reviews {
   stars : number
   contents : string
@@ -15,13 +13,29 @@ interface ReviewsProps {
 }
 
 export default function Reviews({reviews} : ReviewsProps) {
+
+  const renderStars = (stars: number) => (
+    <>
+      {Array(Math.floor(stars))
+        .fill(0)
+        .map((_, index) => (
+          <FaStar key={`full-${index}`} className="text-yellow-500" />
+        ))}
+      {stars % 1 !== 0 && <FaStarHalfAlt className="text-yellow-500" />}
+      {Array(5 - Math.floor(stars) - (stars % 1 !== 0 ? 1 : 0))
+        .fill(0)
+        .map((_, index) => (
+          <FaStar key={`empty-${index}`} className="text-grey-200" />
+        ))}
+    </>
+  );
   return (
     <div id="section3" className="p-2 my-4">
     <h1 className="md:text-2xl text-base">리뷰</h1>
     <div className="mt-4 flex flex-col justify-center items-center">
       {reviews.map((review) => (
         <div key={review.created_at} className="mx-3 border border-slate-400 w-full flex flex-col justify-between h-[100px] md:h-auto md:p-4 p-3 rounded-xl mb-3">
-          <div className="flex flex-row">{review.stars}</div>
+          <div className="flex flex-row">{renderStars(review.stars)}</div>
           <div className="line-clamp-1 md:my-2 my-1">
             <p className="md:text-xl text-xs line-clamp-1">{review.contents}</p>
           </div>
