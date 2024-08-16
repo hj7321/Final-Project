fa833729273463f3ad8095f54b6772ad22620f14
hj7321 <oboki2001@naver.com>
2024-08-16 12:15:51 +0900

    refactor: img 태그 -> Image 태그, 의존성 배열 추가

--- src/app/(provider)/(root)/chat/_components/ChatModal.tsx
+++ src/app/(provider)/(root)/chat/_components/ChatModal.tsx
@@ -101,15 +101,19 @@ const ChatModal: React.FC<ChatModalProps> = ({ chatRoomId, onClose, onMessagesRe
 
     const chatChannel = supabase
       .channel('realtime:chat')
-      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'Chat', filter: `chat_room_id=eq.${chatRoomId}` }, (payload) => {
-        setMessages((prevMessages) => [...prevMessages, payload.new as Chat]);
-      })
+      .on(
+        'postgres_changes',
+        { event: 'INSERT', schema: 'public', table: 'Chat', filter: `chat_room_id=eq.${chatRoomId}` },
+        (payload) => {
+          setMessages((prevMessages) => [...prevMessages, payload.new as Chat]);
+        }
+      )
       .subscribe();
 
     return () => {
       supabase.removeChannel(chatChannel);
     };
-  }, [chatRoomId, currentUser, isRead]);
+  }, [chatRoomId, currentUser, isRead, onMessagesRead]);
 
   useEffect(() => {
     if (messagesEndRef.current) {
@@ -129,8 +133,8 @@ const ChatModal: React.FC<ChatModalProps> = ({ chatRoomId, onClose, onMessagesRe
         pro_id: currentUser.id,
         content: newMessage,
         chat_room_id: chatRoomId,
-        is_read: false, // 새로운 메시지는 읽지 않음으로 표시
-      },
+        is_read: false // 새로운 메시지는 읽지 않음으로 표시
+      }
     ]);
 
     if (error) {
@@ -144,11 +148,19 @@ const ChatModal: React.FC<ChatModalProps> = ({ chatRoomId, onClose, onMessagesRe
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-0 md:p-0">
       <div className="bg-white md:p-5 md:rounded-xl w-full md:w-1/3 max-w-xl h-full md:h-5/6">
-        <button onClick={onClose} className="text-black float-right mr-2 md:mr-0"><Image src="/closeBtnX.svg" alt="닫기버튼" width={20} height={20} /></button>
+        <button onClick={onClose} className="text-black float-right mr-2 md:mr-0">
+          <Image src="/closeBtnX.svg" alt="닫기버튼" width={20} height={20} />
+        </button>
         <div className="flex items-center mb-6 ml-1 md:ml-0">
           {otherUser && (
             <>
-              <img src={otherUser.profile_img || '/defaultProfileimg.svg'} alt="상대 프로필" className="w-12 h-12 rounded-full mr-4" />
+              <Image
+                src={otherUser.profile_img || '/defaultProfileimg.svg'}
+                alt="상대 프로필"
+                width={48}
+                height={48}
+                className="rounded-full mr-4"
+              />
               <div>
                 <h2 className="text-sm font-semibold">{otherUser.nickname}</h2>
                 <p className="text-sm font-medium text-gray-500">연락 가능 시간: AM 9 - PM 6</p>
@@ -164,7 +176,13 @@ const ChatModal: React.FC<ChatModalProps> = ({ chatRoomId, onClose, onMessagesRe
                 key={message.id}
                 className={`mb-2 flex ${message.consumer_id === currentUser?.id ? 'justify-end' : 'justify-start'}`}
               >
-                <div className={`p-3 rounded-lg text-xs max-w-xs font-medium ${message.consumer_id === currentUser?.id ? 'bg-primary-50 border border-primary-100 text-black' : 'bg-gray-50 border border-grey-200 text-black'} break-words`}>
+                <div
+                  className={`p-3 rounded-lg text-xs max-w-xs font-medium ${
+                    message.consumer_id === currentUser?.id
+                      ? 'bg-primary-50 border border-primary-100 text-black'
+                      : 'bg-gray-50 border border-grey-200 text-black'
+                  } break-words`}
+                >
                   {message.content}
                 </div>
               </div>
@@ -180,8 +198,8 @@ const ChatModal: React.FC<ChatModalProps> = ({ chatRoomId, onClose, onMessagesRe
               placeholder="메시지를 입력하세요"
             />
             <button type="submit" className="p-2 bg-primary-500 text-white text-sm font-normal rounded-lg flex p-3">
-              <Image src="/sendMessage.svg" alt="메세지버튼" width={20} height={20} className='text-white'/>
-              <div className='hidden md:block'>보내기</div>
+              <Image src="/sendMessage.svg" alt="메세지버튼" width={20} height={20} className="text-white" />
+              <div className="hidden md:block">보내기</div>
             </button>
           </form>
         </div>
