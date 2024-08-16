b9c50aec9804e5e836206e2bfa482880682385ad
hj7321 <oboki2001@naver.com>
2024-08-16 12:16:57 +0900

    add: 특정한 유저 아이디(uuid)를 통해 유저 정보를 가져오는 커스텀 훅 생성

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,30 @@
+import { Users } from '@/types/type';
+import { useQuery } from '@tanstack/react-query';
+
+export default function useProfile(userId: string | null | undefined) {
+  const getUserData = async () => {
+    const data = await fetch(`/api/users/${userId}`, {
+      method: 'GET'
+    }).then((res) => res.json());
+
+    if (data.errorMsg) {
+      console.log('유저 데이터 못가져옴: ', data.errorMsg);
+      return null;
+    } else {
+      console.log('유저 데이터 가져옴');
+      return data.userData;
+    }
+  };
+
+  const {
+    data: userData,
+    isPending: isUserDataPending,
+    error: userDataError
+  } = useQuery<Users | null>({
+    queryKey: ['User', userId],
+    queryFn: getUserData,
+    enabled: !!userId
+  });
+
+  return { userData, isUserDataPending, userDataError };
+}
